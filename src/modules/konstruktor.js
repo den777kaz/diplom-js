bacc44ac44149f0b3e2529bf83110661e006440d
Denis Kazakov <k19den85@gmail.com>
2019-08-10 14:06:45 +0200

    konstruktor

--- src/modules/konstruktor.js
+++ src/modules/konstruktor.js
@@ -1,41 +1,78 @@
-
 const konstruktor = () => {
 
-        const onOff = document.getElementById('myonoffswitch'),
-            panelGroup = document.getElementById('accordion'),
-           headPanels = document.querySelectorAll('.panel-heading');
-
-        // TEXTPANELS
-        const textPanels = document.querySelectorAll('.collapse');
-        const collapseTwo = document.getElementById('collapseTwo');
-        const collapseThree = document.getElementById('collapseThree');
-        const collapseVour = document.getElementById('collapseVour');
+    const onOff = document.getElementById('myonoffswitch'),
+        panelGroup = document.getElementById('accordion'),
+        headPanels = document.querySelectorAll('.panel-head'),
+        construktBtn = document.querySelectorAll('.construct-btn');
 
 
-            };
-                export default konstruktor;
+    // TEXTPANELS
+    const textPanels = document.querySelectorAll('.text-panels'),
+        collapseOne = document.getElementById('collapseOne'),
+        collapseTwo = document.getElementById('collapseTwo'),
+        collapseThree = document.getElementById('collapseThree'),
+        collapseFour = document.getElementById('collapseFour');
 
 
-                // const toggleTabContent = (index) => {
-                //     for (let i = 0; i < panelText.length; i++) {
-                //         if (index === i) {
-                //             panelText[i].style.display = 'block';
-                //         } else {
-                //             panelText[i].style.display = 'none';
-                //         }
-                //     }
-                // };
+    const nextTab = () => {
+        // next-btn-one
+        construktBtn.forEach((item)=> {
+            item.addEventListener('click', (event)=> {
+                let target = event.target;
+                event.preventDefault();
+                if(target.closest('.next-btn-one')){
 
-                // accordionGroup.addEventListener('click', (event) => {
-                //     event.preventDefault();
+                    collapseTwo.style.display = 'block';
+                    collapseOne.style.display = 'none';
 
-                //     let target = event.target;
-                //     target = target.closest('.panel-heading');
+                 }else if (target.closest('.next-btn-two')){
 
-                //     if (target.classList.contains('panel-heading')) {
-                //         panelHead.forEach((item, i) => {
-                //             if (item === target) {
-                //                 toggleTabContent(i);
-                //             }
-                //         });
-                //     }
+                    collapseThree.style.display = 'block';
+                    collapseTwo.style.display = 'none';
+
+                 }else if (target.closest('.next-btn-three')) {
+
+                    collapseFour.style.display = 'block';
+                    collapseThree.style.display = 'none';
+                 }
+            });
+          
+        });
+
+
+       
+
+    };
+    nextTab();
+
+
+    const switchContent = (index) => {
+        for (let i = 0; i < textPanels.length; i++) {
+            if (index === i) {
+                textPanels[i].style.display = 'block';
+            } else {
+                textPanels[i].style.display = 'none';
+            }
+        }
+    };
+
+    panelGroup.addEventListener('click', (event) => {
+
+        let target = event.target;
+
+        target = target.closest('.panel-head');
+
+        if (!target) {
+            return;
+        } else {
+            event.preventDefault();
+            headPanels.forEach((item, i) => {
+                if (item === target) {
+                    switchContent(i);
+                }
+            });
+        }
+    });
+
+};
+export default konstruktor;
