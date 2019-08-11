06a193d968cf0e690f8ebd85d9a602904b5b9fd9
Denis Kazakov <k19den85@gmail.com>
2019-08-11 12:44:41 +0200

    calc problem

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -16,12 +16,107 @@ const calculator = () => {
         ringAmountTwo = document.querySelector('.ring-amount-two');
 
 
+    // const calcData = {
+    //     totalResult: 0,
+    //     oneBarrelPrice: 10000,
+    //     twoBarrelPrise: 15000,
+    //     diameterBarrelOne: '',
+    //     diameterBarrelTwo: '',
+    //     amountBarrelOne: '',
+    //     amountBarrelTwo: '',
+    //     ringPrice: 0,
+    //     ringPriceTwo: 0,
+    //     diameterPrice: 0,
+    //     diameterPriceTwo: 0,
+    //     start: function () {
+    //         this.getDiameterAmountOne();
+    //         this.showResult();
+    //     },
+    //     getDiameterAmountOne: function () {
+    //         const diameterValue = diameterOne.options[diameterOne.selectedIndex];
+    //         const ringsValue = ringAmountOne.options[ringAmountOne.selectedIndex];
+    //         const diameterValueTwo = diameterTwo.options[diameterTwo.selectedIndex].index;
+    //         const ringsValueTwo = ringAmountTwo.options[ringAmountTwo.selectedIndex].index;
+    //         console.log(diameterOne.options);
+    //         // FIRST 
+
+    //         diameterOne.addEventListener('change', ()=>{
+
+    //                 if (diameterValue === 1) {
+    //                     this.diameterBarrelOne = diameterValue.text;
+    //                     this.diameterPrice = 10000 * 0.2;
+
+    //                 } else {
+    //                     this.diameterBarrelOne = diameterValue.text;
+    //                     this.diameterPrice = 0;
+    //                 }
+
+    //         });
+    //             if (diameterValue === 1) {
+    //                 this.diameterBarrelOne = diameterValue.text;
+    //                 this.diameterPrice = 10000 * 0.2;
+    //                 console.log('this.diameterPrice: ', this.diameterPrice);
 
 
+    //             } else {
+    //                 this.diameterBarrelOne = diameterValue.text;
+    //                 this.diameterPrice = 0;
+    //             }
+
+
+
+    //         if (diameterValue.index === 1) {
+    //             this.diameterBarrelOne = diameterValue.text;
+    //             this.diameterPrice = 10000 * 0.2;
+    //             console.log('this.diameterPrice: ', this.diameterPrice);
+
+
+    //         } else {
+    //             this.diameterBarrelOne = diameterValue.text;
+    //             this.diameterPrice = 0;
+    //         }
+
+
+    //         if (ringsValue.index === 1) {
+    //             this.ringPrice = 10000 * 0.3;
+    //             this.amountBarrelOne = ringsValue.text;
+    //         } else if (ringsValue.index === 2) {
+    //             this.amountBarrelOne = ringsValue.text;
+    //             this.ringPrice = 10000 * 0.5;
+    //         } else if (ringsValue.index === 0) {
+    //             this.amountBarrelOne = ringsValue.text;
+    //             this.ringPrice = 0;
+    //         }
+
+
+    //     },
+    //     showResult: function () {
+    //         this.result = this.oneBarrelPrice +  this.diameterPrice + this.ringPrice;
+
+    //         calcResult.value = this.result;
+    //     }
+
+
+    // };
+
+    // calcData.start();
+
+    // console.log(calcData);
+
+
+    // ringAmountOne.addEventListener('change', calcData.getDiameterAmountOne);
+    // ringAmountTwo.addEventListener('change', calcData.getDiameterAmountOne);
+    // diameterTwo.addEventListener('change', calcData.getDiameterAmountOne);
+    // // onOffTwo.addEventListener('change', getResult);
+    // // onOff.addEventListener('change', getResult);
+
+
+
+    // WORKING BUT BAD CODE!!!!
     function getResult() {
         let result = 0;
         let oneBarrel = 10000;
-        let twoBarrel = 10000;
+        let twoBarrel = 15000;
         let hole = 0;
         let ringPrice;
         let ringPriceTwo;
@@ -67,11 +162,11 @@ const calculator = () => {
 
 
         // SWITCH ONE OR TWO
-        const switchBarrel = ()=> {
+        const switchBarrel = () => {
 
             allSelects[2].disabled = true;
             allSelects[3].disabled = true;
-      
+
             if (onOff.checked) {
                 allSelects[2].disabled = true;
                 allSelects[3].disabled = true;
@@ -81,23 +176,31 @@ const calculator = () => {
             }
         };
         switchBarrel();
-      
+
 
 
         //ON OFF 
 
         if (onOffTwo.checked) {
-           if(onOff.checked){
-               hole = 1000;
-           }else {
-               hole = 2000;
-           }
+
+            if (onOff.checked) {
+                hole = 1000;
+                result = oneBarrel + diameterPrice + ringPrice + hole;
+            } else {
+                hole = 2000;
+                result = oneBarrel + twoBarrel + diameterPrice + ringPrice + diameterPriceTwo + ringPriceTwo + hole;
+            }
         } else {
             hole = 0;
         }
 
         // RESULT
-        result = oneBarrel + diameterPrice + ringPrice + diameterPriceTwo + ringPriceTwo + hole;
+        if (onOff.checked) {
+            result = oneBarrel + diameterPrice + ringPrice + hole;
+        } else if (!onOff.checked) {
+            result = oneBarrel + twoBarrel + diameterPrice + ringPrice + diameterPriceTwo + ringPriceTwo + hole;
+        }
+
         calcResult.value = result;
     }
     diameterOne.addEventListener('change', getResult);
